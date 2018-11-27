98cde48845bbaba5cd923c65f0084ed29ac2e153
Raylene <smith.raylene@gmail.com>
2018-11-27 11:19:40 +0000

    Set up devise
    Co-Authored-By: alfie <runticle@gmail.com>
    Co-Authored-By: shannon <shannongamby@hotmail.com>
    Co-Authored-By: todd <toddpla@gmail.com>
    Co-Authored-By: imogen <imogenmisso@hotmail.co.uk>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
+<<<<<<< HEAD
 class ApplicationController < ActionController::API
   # protect_from_forgery with: :exception
   include SessionsHelper
   include Response
   include ExceptionHandler
+=======
+class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+  include Response
+  include ExceptionHandler
+>>>>>>> set up devise
 
-  before_action :require_login
+  protect_from_forgery with: :exception
 
   def redirect_to_login
     redirect_to login_url
